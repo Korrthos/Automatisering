cb9a39f304c9753f2021e3c3d88657fb659276fb
Korrthos <johannes.monrad@gmail.com>
2023-02-08 13:31:58 +0100

    integrated with serial communication

--- src/color.py
+++ src/color.py
@@ -1,85 +1,80 @@
-# Python code for Multiple Color Detection
-# https://www.geeksforgeeks.org/multiple-color-detection-in-real-time-using-python-opencv/
-
-
-  
+import serial
 import numpy as np
 import cv2
 
+# serial communication
+Serial = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+Serial.reset_input_buffer()
+
 # color array
-colors = ["red", "blue", "green", "blue", "orange", "brown"]
+colors = ["RED", "BLUE", "GREEN", "YELLOW", "ORANGE", "BROWN"]
 
 # make color dicts:
 colors_lower = {
-    "red":      [  0, 174, 119 ],
-    "green":    [ 58, 54,  79  ],
-    "blue":     [ 94, 216, 78  ],
-    "yellow":   [  8, 90,  142 ],
-    "orange":   [  0, 131, 150 ],
-    "brown":    [  0, 113,  51 ],
+    "RED":      [  0, 174, 119 ],
+    "GREEN":    [ 58, 54,  79  ],
+    "BLUE":     [ 94, 216, 78  ],
+    "YELLOW":   [  8, 90,  142 ],
+    "ORANGE":   [  0, 131, 150 ],
+    "BROWN":    [  0, 113,  51 ],
 }
 
 colors_upper = {
-    "red":      [ 15, 214, 199 ],
-    "green":    [ 78,  94, 159 ],
-    "blue":     [114, 255, 158 ],
-    "yellow":   [ 28, 130, 222 ],
-    "orange":   [ 20, 171, 230 ],
-    "brown":    [ 17, 153, 131 ]
+    "RED":      [ 15, 214, 199 ],
+    "GREEN":    [ 78,  94, 159 ],
+    "BLUE":     [114, 255, 158 ],
+    "YELLOW":   [ 28, 130, 222 ],
+    "ORANGE":   [ 20, 171, 230 ],
+    "BROWN":    [ 17, 153, 131 ]
 }
 
-colors_disp = {
-    "red":      (   97, 97, 248 ),
-    "green":    (  91, 216, 56  ),
-    "blue":     ( 208, 129, 15  ),
-    "yellow":   (   0, 217, 253 ),
-    "orange":   (  54, 148, 255 ),
-    "brown":    ( 102, 102, 155 ),
-}
-
+# get black & white mask
 def makeMask(hsvFrame, color):
     color_lower = np.array(colors_lower[color], np.uint8)
     color_upper = np.array(colors_upper[color], np.uint8)
     mask = cv2.inRange(hsvFrame, color_lower, color_upper)
     return mask
 
-def morph(imageFrame, mask, kernal):
-    mask = cv2.dilate(mask, kernal)
-    result = cv2.bitwise_and(imageFrame, imageFrame, mask = mask)
-    return mask, kernal
-
-def makeContour(imageFrame, mask, color):
-    col = colors_disp[color]
+# use black and white mask to check if the areas of a specific mask is over 300.
+def makeContour(mask):
+    found = 0
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for pic, contour in enumerate(contours):
         area = cv2.contourArea(contour)
         if(area > 300):
-            x, y, w, h = cv2.boundingRect(contour)
-            imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), col, 2)
-            cv2.putText(imageFrame, f"{color} color", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, col)
-    return imageFrame
+            found += 1
 
+    return found
 
-
-# Capturing video through webcam
 webcam = cv2.VideoCapture(0)
 
-# Start a while loop
+found = dict(zip( [color for color in colors], [0 for _ in range(6)] ))
+# found = {
+#     "RED":    0,
+#     "GREEN":  0,
+#     "BLUE":   0,
+#     "YELLOW": 0,
+#     "ORANGE": 0,
+#     "BROWN":  0
+# }
+
+FRAME_CHECK = 10
+frame_counter = 0
 while(True):
-      
-    # Reading the video from the
-    # webcam in image frames
+    
     _, imageFrame = webcam.read()
-    # nimageFrame = cv2.resize(imageFrame, (0, 0), fx = 1, fy = 1);
     hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
-    cv2.imshow('hsv', hsvFrame)
     for color in colors:
         mask = makeMask(hsvFrame, color)
-        imageFrame = makeContour(imageFrame, masks[color], color)
+        found[color] = makeContour(mask)
 
-    # Program Termination
-    cv2.imshow("contours", imageFrame)
+    frame_counter = (frame_counter + 1) % FRAME_CHECK 
+    if frame_counter == 0:
+        for (color, val) in found.items():
+            if val > 6:
+                Serial.write(bytes(color, 'utf-8'))
+                found[color] = 0
+            
     if cv2.waitKey(1) & 0xFF == ord('q'):
         webcam.release()
-        cv2.destroyAllWindows()
         break
